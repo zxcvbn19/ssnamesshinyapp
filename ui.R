4443adbe4f4d16d88dc29094c7189a774cd7a86e
zxcvbn19 <47610734+zxcvbn19@users.noreply.github.com>
2019-03-08 13:54:05 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -24,18 +24,3 @@ shinyUI(fluidPage(
 )
 ))
 
-#### To do:
-
-
-#Create switch that returns empty plot with overlaid text that says "No Results Found" if
-#a name shows up with no matches.
-
-#See if that gets us a working plot, and fiddle with the ui features to make it look pretty
-
-#STOP: Put the images in your power point.
-
-#Work on the "This name was the Xth most popular name that year"
-
-#delve into leaflet options, see if we can get a working output at all.
-
-#doesn't have to look pretty, just has to be something
